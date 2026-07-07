9a3607d6c5a1473d41413e659d7745f79db3f6b0
Zlatko Bratkovic <zbratkovic@haproxy.com>
2026-07-07 10:31:14 +0200

    MINOR: add built-in C and Rust dictionaries
    Introduce new embedded dictionaries containing common keywords, types,
    and tooling names for C (c.txt) and Rust (rust.txt).

--- aspell/dictionaries_test.go
+++ aspell/dictionaries_test.go
@@ -4,7 +4,8 @@ import "testing"
 
 func TestEmbeddedLanguageDictionariesLoaded(t *testing.T) {
 	// One representative word per added wordlist.
-	for _, word := range []string{"gomemlimit", "gotoolchain", "kwargs", "asyncio", "pinia", "composable", "testcontainers", "leaderelection", "mgmt", "srvtcpka"} {
+	//revive:disable-next-line:line-length-limit
+	for _, word := range []string{"gomemlimit", "gotoolchain", "kwargs", "asyncio", "pinia", "composable", "testcontainers", "leaderelection", "mgmt", "srvtcpka", "uintptr_t", "valgrind", "borrowck", "clippy"} {
 		if _, ok := acceptableWordsGlobal[word]; !ok {
 			t.Errorf("expected %q loaded from embedded dictionaries", word)
 		}
